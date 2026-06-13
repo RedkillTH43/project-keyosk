9e21e0706de8669dca4492ab5d6a0952fe3443b9
RedkillTH43 <johnlawrencetamio@gmail.com>
2026-06-13 16:35:05 +0800

    main.py: Added new page to pick meal type, not functional yet.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ root.geometry("600x800")
 
 # Declare constant variables
 ITEM_NUM = 7
-category_names_arr = ["Home", "Breakfast", "Lunch", "Dinner", "Sides", "Desserts", "Drinks"]
 
-# Declares arrays to hold widgets
+# Declares arrays
+category_names_arr = ["Home", "Breakfast", "Lunch", "Dinner", "Sides", "Desserts", "Drinks"]
 categories_arr = []
 items_arr = []
 
@@ -27,32 +27,71 @@ def change_category(_category_name):
     # Checks category and assign accordingly
     match _category_name:
         case "Home":
-            print("You clicked Home")
             item_amount = 1
         case "Breakfast":
-            print("You clicked Breakfast")
             item_amount = 2
         case "Lunch":
-            print("You clicked Lunch")
             item_amount = 3
         case "Dinner":
-            print("You clicked Dinner")
             item_amount = 4
         case "Sides":
-            print("You clicked Sides")
             item_amount = 5
         case "Desserts":
-            print("You clicked Desserts")
             item_amount = 6
         case "Drinks":
-            print("You clicked Drinks")
             item_amount = 7
         case _:
-            print("Invalid")
             item_amount = 8
 
     make_item_cards(item_amount)
-    update()
+    update_items()
+
+def reset_main():
+    main_frm.columnconfigure(0, weight=1)
+    main_frm.columnconfigure(1, weight=1)
+
+# Remove all frames in the screen except for main
+def clear_screen():
+    for frame in frames_dict:
+        if frame == "main":
+            continue
+        frames_dict[frame].destroy()
+
+# Switch to page asking for meal type
+def pick_meal_type():
+    clear_screen()
+    reset_main()
+
+    main_frm.rowconfigure(0, weight=0)
+    main_frm.rowconfigure(1, weight=0)
+    main_frm.rowconfigure(2, weight=1)
+
+    question_lbl = tk.Label(main_frm, text="Would you like to make it a meal?", font=("Arial", 20), pady=5)
+    yes_btn = tk.Button(
+        main_frm,
+        text="Yes, make it a meal",
+        cursor="hand2",
+        image=item_image,
+        compound="top",
+        borderwidth=2,
+        relief="solid",
+        height=300
+    )
+    no_btn = tk.Button(
+        main_frm,
+        text="No, ala carte only",
+        cursor="hand2",
+        image=item_image,
+        compound="top",
+        borderwidth=2,
+        relief="solid",
+        height=300
+    )
+
+    question_lbl.grid(row=1, column=0, columnspan=2, sticky="n")
+    yes_btn.grid(row=2, column=0, sticky="new", padx=5, pady=5)
+    no_btn.grid(row=2, column=1, sticky="new", padx=5, pady=5)
+
 
 # Create frames to hold widgets
 main_frm = tk.Frame(root)
@@ -63,6 +102,7 @@ category_frm = tk.Frame(main_frm)
 item_frm = tk.Frame(main_frm)
 button_frm = tk.Frame(main_frm, padx=5)
 
+# Hold frames in a dictionary for quick access
 frames_dict = {
     "main": main_frm,
     "category": category_frm,
@@ -100,7 +140,8 @@ def make_item_cards(amount):
             image=item_image,
             compound="top",
             borderwidth=2,
-            relief="solid"
+            relief="solid",
+            command=pick_meal_type
         )
         items_arr.append(item_btn)
 make_item_cards(ITEM_NUM)
@@ -109,7 +150,7 @@ make_item_cards(ITEM_NUM)
 heading_lbl = tk.Label(main_frm, text="Keyosk", font=("Arial", 40), pady=5)
 
 # Display widgets
-heading_lbl.grid(row=0, column=0, columnspan=2)
+heading_lbl.grid(row=0, column=0, columnspan=2, sticky="n")
 frames_dict["category"].grid(row=1, column=0, sticky="nsew")
 frames_dict["item"].grid(row=1, column=1, sticky="nsew")
 frames_dict["button"].grid(row=2, column=0, columnspan=2, pady=10)
@@ -119,7 +160,7 @@ for category in categories_arr:
     category.pack(anchor="w", pady=5, padx=5)
 
 # Display item widgets
-def update():
+def update_items():
     item_row = 0
     item_column = 0
 
@@ -130,13 +171,13 @@ def update():
         item.grid(row=item_row, column=item_column, padx=5, pady=5)
         item_column += 1
     frames_dict["item"].grid(row=1, column=1, sticky="n")
-update()
+update_items()
 
 # Display buttons
 cancel_btn.pack(side="left", padx=5)
 done_btn.pack(side="left", padx=5)
 
-frames_dict["main"].pack(expand=True, fill="both")
+frames_dict["main"].pack(expand=True, fill="both", padx=10)
 
 # Ensures program keeps running
 root.mainloop()
